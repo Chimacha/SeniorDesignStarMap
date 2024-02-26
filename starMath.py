6b4e3913f14dc9bbf844cbad068d9b5beeb3ad2a
Chimacha <chinagunter@me.com>
2024-02-26 15:12:57 -0600

    Create starMath.py

---
+++ starMath.py
@@ -0,0 +1,47 @@
+def jdToDate(JD):
+    #converts julian date to normal date
+    return
+
+def getGST(time):
+    #returns greenwich sidereal time
+    return
+
+def getStarAzEl(ra, dec, gst):
+    #gst will be passed in
+    #math
+    #return azimuth and elevation
+    return
+
+def getPlanetRADec(soManyThings):
+    #planet math. Can be run individually for each planet
+    #cy = JD / 36525 where JD is julian date counted from 2000
+    #so much math
+    #returns RA and Dec of the planet
+    return
+
+def getPlanetAzEl(ra, dec):
+    #not sure if this is different from star RA and Dec but we'll see I guess
+    #planet math
+    #returns planet azimuth and elevation
+    return
+
+def raDegToHMS(deg):
+    #converts RA from degrees to hours:minutes:seconds
+    return
+
+def decDegToDMS(deg):
+    #converts declination from degrees to degree:minutes:seconds
+    return
+
+def getMoonPhase(time, julianDate, location):
+    #gets moon phase
+    #moon math
+    #return the phase based on a threshold and display the proper png based on that
+    return
+
+def getMoonLocation(T):
+    #gets moon's location
+    #T is explained in the doc
+    #those are some ugly formulas :((
+    #return moon's geocentric longitude and latitude
+    return
