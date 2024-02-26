d0ad7884adbcbec1355abbf699ee06cc221f0e75
Chimacha <chinagunter@me.com>
2024-02-26 15:10:53 -0600

    Create mapPlot.py

---
+++ server/mapPlot.py
@@ -0,0 +1,12 @@
+import matplotlib
+from starMath import *
+
+def getStarInfo():
+    #I assume this will be needed to hold some info? Could be useless/extraneous idk
+    return
+
+def drawMap():
+    #the horrors
+    #create and save png
+    #returns png
+    return
